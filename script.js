a1fff2aa5ec47796310586db50eb19243457817e
Cosmin Negrut <cosminnegrut2002@gmai.com>
2022-03-23 23:33:00 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
-document.querySelector('.number').textContent = 23;
-document.querySelector('.score').textContent = 23;
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent = 23;
+// document.querySelector('.score').textContent = 23;
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  }
+});
