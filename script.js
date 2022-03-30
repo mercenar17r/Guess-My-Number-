2e8f60ae35f208434dc9b8fa9439d4a96564079b
Cosmin Negrut <cosminnegrut2002@gmai.com>
2022-03-30 22:19:29 +0300

    .

--- script.js
+++ script.js
@@ -6,10 +6,15 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
   }
 });
